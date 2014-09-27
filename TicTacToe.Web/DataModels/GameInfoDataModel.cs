cadff5c06e6d618c2652b49ff79e0d4f62a8e9da
StefanSinapov <stefan.sinapov@gmail.com>
2014-09-27 15:43:25 +0300

    DataModels - added

--- TicTacToe.Web/DataModels/GameInfoDataModel.cs
+++ TicTacToe.Web/DataModels/GameInfoDataModel.cs
@@ -1,7 +1,7 @@
 ﻿namespace TicTacToe.Web.DataModels
 {
     using System;
-
+    using System.ComponentModel.DataAnnotations;
     using AutoMapper;
 
     using TicTacToe.Models;
@@ -10,6 +10,12 @@
     public class GameInfoDataModel : IMappableFrom<Game>, IHaveCustomMappings
     {
         public Guid Id { get; set; }
+
+        [MinLength(3)]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public DateTime DateCreated { get; set; }
 
         public string FirstPlayerName { get; set; }
 
@@ -22,6 +28,8 @@
         public void CreateMappings(IConfiguration configuration)
         {
             configuration.CreateMap<Game, GameInfoDataModel>()
+                .ForMember(m => m.Name, opt => opt.MapFrom(game => game.Name))
+                .ForMember(m => m.DateCreated, opt => opt.MapFrom(game => game.DateCreated))
                 .ForMember(m => m.FirstPlayerName, opt => opt.MapFrom(game => game.FirstPlayer == null ? null : game.FirstPlayer.UserName))
                    .ForMember(m => m.SecondPlayerName, opt => opt.MapFrom(game => game.SecondPlayer == null ? null : game.SecondPlayer.UserName))
                    .ForMember(m => m.State, opt => opt.MapFrom(game => game.State.ToString()));
---
+++ TicTacToe.Web/DataModels/UserDataModel.cs
@@ -0,0 +1,21 @@
+﻿namespace TicTacToe.Web.DataModels
+{
+    using System;
+    using AutoMapper;
+
+    using Mapping;
+    using TicTacToe.Models;
+
+    public class UserDataModel : IMappableFrom<User>, IHaveCustomMappings
+    {
+        public string Username { get; set; }
+        public DateTime DateRegistration { get; set; }
+
+        public void CreateMappings(IConfiguration configuration)
+        {
+            configuration.CreateMap<User, UserDataModel>()
+                .ForMember(m => m.Username, opt => opt.MapFrom(user => user.UserName))
+                .ForMember(m => m.DateRegistration, opt => opt.MapFrom(user => user.DateRegistration));
+        }
+    }
+}
